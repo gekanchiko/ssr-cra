a89ed42ff399fe8f5028cccb7046357cf248b782
Evgenij Olshanskij <evgenij@selina.com>
2019-09-02 11:32:21 +0300

    Remove AsyncComponent from the tree

--- src/client/utils/index.js
+++ src/client/utils/index.js
@@ -1,4 +1,3 @@
 export { default as Module } from './Module';
 export { default as loadModules } from './loader';
 export { default as io } from './io';
-export { default as AsyncComponent } from './AsyncComponent';
